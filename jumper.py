9ff2fc1f4f88bc67eb873bdd16d4d464141ebecd
FiercelyPierce <cir21001@byui.edu>
2022-05-19 21:26:54 -0700

    Started building the skeleton

---
+++ jumper.py
@@ -0,0 +1,22 @@
+# cse210 Team 4 Week 5
+
+class Jumper:
+    '''
+    
+    '''
+    
+
+    def __init__(self):
+        '''Constructs a new Jumper.
+        
+        Args:
+            self (Jumper): an instance of Jumper.
+        '''
+
+    def register_miss(self):
+        '''
+        '''
+    
+    def is_alive(self):
+        '''
+        '''
