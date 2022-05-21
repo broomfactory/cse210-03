20f95ae40dee41767ab9d4272a0d7954a1fec530
DesertShaddow <desertshaddow@gmail.com>
2022-05-21 11:01:51 -0700

    Adding comments and fixing code.

--- WordList.py
+++ WordList.py
@@ -1,12 +1,17 @@
 import random
+from webbrowser import get
 
 class WordList: 
 
-    def __init__(self) -> None:
+    "This class creates a list of words for the hide and seek game."
+
+    def __init__(self):
         
         self._WordList = ["word", "apple", "banana", "sheep", "spade", "crisp", "chips", "cars", "orange", "shoes", "grasses", "laptop", "tables"]
+       
+    def get_random_word(self):
 
-    def getRandomWord(word):
+        "This definition chooses a word from the list for the puzzle class secret word attribute to grab."
 
-        word = random.choice(WordList)
+        word = random.choice(self._WordList)
         return word
