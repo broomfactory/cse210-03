65340d8867804a489426ab788f2fa6910ea0ae09
FiercelyPierce <cir21001@byui.edu>
2022-05-25 17:13:34 -0700

    Almost done with the methods

--- jumper.py
+++ jumper.py
@@ -12,11 +12,36 @@ class Jumper:
         Args:
             self (Jumper): an instance of Jumper.
         '''
+        self._parachute = [
+            '  ___  ', 
+            ' /   \ ', 
+            ' \   / ', 
+            '  \ /  ', 
+            '   O   '
+            ]
+
+    def draw_jumper(self):
+        '''
+        '''
+        for i in self._parachute:
+            if len(self._parachute) > 0:
+                print(i)
+            else:
+                print('   X   ')
+
+            print('  /|\  ')
+            print('  / \  ')
 
     def register_miss(self):
         '''
+            Removes the first section from the parachute
         '''
+        self._parachute.pop()
     
     def is_alive(self):
         '''
         '''
+        if len(self._parachute) == 0:
+            return True
+        else:
+            return False
